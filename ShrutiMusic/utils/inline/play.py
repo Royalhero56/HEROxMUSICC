f80e6a4ae8139609fe9872f983e830ff9d96e746
L2LUCKY <145443484+L2LUCKY@users.noreply.github.com>
2025-11-03 11:01:07 +0530

    Update play.py

--- ShrutiMusic/utils/inline/play.py
+++ ShrutiMusic/utils/inline/play.py
@@ -23,7 +23,7 @@
 import math
 from pyrogram.types import InlineKeyboardButton
 from ShrutiMusic.utils.formatters import time_to_seconds
-from config import BOT_USERNAME, OWNER_ID, SUPPORT_CHANNEL, SUPPORT_GROUP
+from config import BOT_USERNAME, SUPPORT_GROUP, SUPPORT_CHANNEL
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
@@ -89,8 +89,8 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
-            InlineKeyboardButton(text="💞ᴏᴡɴᴇʀ💞", user_id=config.OWNER_ID),
-            InlineKeyboardButton(text="💓ʙʀᴏᴛʜᴇʀ💓", url=SUPPORT_CHANNEL),
+            InlineKeyboardButton(text="💬 sᴜᴘᴘᴏʀᴛ", url=SUPPORT_GROUP),
+            InlineKeyboardButton(text="📢 ᴄʜᴀɴɴᴇʟ", url=SUPPORT_CHANNEL),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
